d6c9fd78105746fda1a10d59b9af8fc3ecfe22f5
Peter Li <lihaohong6@gmail.com>
2024-05-29 21:22:16 -0400

    still reproducible after some simplifying changes

--- test.cpp
+++ test.cpp
@@ -1,27 +1,34 @@
 #include "parlay/alloc.h"
-#include "utils/simple_queue.h"
+#include <queue>
+#include <mutex>
 
 constexpr size_t TOTAL_SIZE = 1UL << 38;
 constexpr size_t BLOCK_SIZE = 1 << 14;
-constexpr size_t QUEUE_COUNT = 128;
 
 struct Test {
     unsigned char data[BLOCK_SIZE];
 };
 using Allocator = parlay::type_allocator<Test>;
 
-struct Writer {
+struct Consumer {
 
-    SimpleQueue<Test*> request_queue;
+    std::queue<Test*> request_queue;
+    std::mutex mutex;
     std::thread t;
 
-    static void thread(Writer *writer) {
+    static void ConsumerThread(Consumer *consumer) {
         size_t freed = 0;
+        auto *queue = &consumer->request_queue;
         while (freed < TOTAL_SIZE) {
             while (true) {
-                auto data = writer->request_queue.Poll();
+                std::lock_guard<std::mutex> lock(consumer->mutex);
+                if (queue->empty()) {
+                    continue;
+                }
+                Test* data = queue->front();
+                queue->pop();
                 if (data != nullptr) {
-                    Allocator::free(data);
+                    // Allocator::free(data);
                     freed += BLOCK_SIZE;
                 } else {
                     break;
@@ -30,20 +37,21 @@ struct Writer {
         }
     }
 
-    Writer() {
-        t = std::thread(thread, this);
+    Consumer() {
+        t = std::thread(ConsumerThread, this);
     }
 
-    ~Writer() {
+    ~Consumer() {
         t.join();
     }
 
     void Add(void *ptr) {
-        request_queue.Push((Test*)ptr);
+        std::lock_guard<std::mutex> lock(mutex);
+        request_queue.push((Test*)ptr);
     }
 };
 
-Writer *writer;
+Consumer *writer;
 
 void thread() {
     auto *ptr = Allocator::alloc();
@@ -52,12 +60,9 @@ void thread() {
 }
 
 int main() {
-    writer = new Writer();
+    writer = new Consumer();
     parlay::parallel_for(0, TOTAL_SIZE / BLOCK_SIZE, [&](size_t i){
         thread();
     }, 1);
-    for (size_t i = 0; i < QUEUE_COUNT; i++) {
-        writer->request_queue.Close();
-    }
     delete writer;
 }
