cfc1fd3d01713aef35a729b2d146de03793f6d39
Peter Li <lihaohong6@gmail.com>
2024-05-29 21:14:30 -0400

    reproducible seg fault in test.cpp

--- test.cpp
+++ test.cpp
@@ -3,30 +3,28 @@
 #include "utils/simple_queue.h"
 
 constexpr size_t TOTAL_SIZE = 1UL << 38;
-constexpr size_t WRITE_SIZE = 1 << 14;
+constexpr size_t BLOCK_SIZE = 1 << 14;
 constexpr size_t QUEUE_COUNT = 128;
 
 struct Writer {
     struct Test {
-        unsigned char data[WRITE_SIZE];
+        unsigned char data[BLOCK_SIZE];
     };
     using Allocator = parlay::type_allocator<Test>;
 
-    SimpleQueue<Test*> queue[QUEUE_COUNT];
+    SimpleQueue<Test*> request_queue;
     std::thread t;
 
     static void thread(Writer *writer) {
         size_t freed = 0;
         while (freed < TOTAL_SIZE) {
-            for (size_t i = 0; i < QUEUE_COUNT; i++) {
-                while (true) {
-                    auto data = writer->queue[i].Poll();
-                    if (data != nullptr) {
-                        Allocator::free(data);
-                        freed += WRITE_SIZE;
-                    } else {
-                        break;
-                    }
+            while (true) {
+                auto data = writer->request_queue.Poll();
+                if (data != nullptr) {
+                    Allocator::free(data);
+                    freed += BLOCK_SIZE;
+                } else {
+                    break;
                 }
             }
         }
@@ -39,29 +37,31 @@ struct Writer {
     ~Writer() {
         t.join();
     }
+
+    void Add(void *ptr) {
+        request_queue.Push((Test*)ptr);
+    }
 };
 Writer *writer;
 
 struct Test {
-    unsigned char data[WRITE_SIZE];
+    unsigned char data[BLOCK_SIZE];
 };
 using Allocator = parlay::type_allocator<Test>;
 
 void thread() {
-    parlay::random_generator gen;
-    std::uniform_int_distribution<size_t> dis(0, QUEUE_COUNT - 1);
     auto *ptr = Allocator::alloc();
-    memset(ptr, 3, WRITE_SIZE);
-    writer->queue[dis(gen)].Push((Writer::Test*)ptr);
+    memset(ptr, 3, BLOCK_SIZE);
+    writer->Add(ptr);
 }
 
 int main() {
     writer = new Writer();
-    parlay::parallel_for(0, TOTAL_SIZE / WRITE_SIZE, [&](size_t i){
+    parlay::parallel_for(0, TOTAL_SIZE / BLOCK_SIZE, [&](size_t i){
         thread();
-    });
+    }, 1);
     for (size_t i = 0; i < QUEUE_COUNT; i++) {
-        writer->queue[i].Close();
+        writer->request_queue.Close();
     }
     delete writer;
 }
