cb21965f8231bb12bfe9e3157f2b392818eba101
Peter Li <lihaohong6@gmail.com>
2024-07-20 20:25:44 -0400

    run ssd scaling test

--- scripts/test_ssd_scaling.sh
+++ scripts/test_ssd_scaling.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+[ $# -ne 2 ] && echo "Usage ${0} start end" && exit 1
+
 bazel build -c opt "//:sample_sort"
 
 executable="./bazel-bin/sample_sort"
@@ -7,7 +9,8 @@ input_prefix="numbers"
 output_prefix="result"
 data_size="34"
 
-for ssd_count in {1..6}; do
+for ssd_count in $(seq "$1" "$2"); do
+  echo "Testing with ${ssd_count} random SSDs"
   bash scripts/clear.sh
   sudo bash scripts/fstrim.sh
   "${executable}" "--num_ssd=${ssd_count}" "--ssd_selection=r" gen "${data_size}" "${input_prefix}" 1 > "temp.txt" 2>&1
