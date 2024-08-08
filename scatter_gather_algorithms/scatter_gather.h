cf96cf4fabac836422a7a319fb17cbe0303b77fb
Peter Li <lihaohong6@gmail.com>
2024-08-07 21:52:25 -0400

    put both phase 2 implementations into scatter gather

--- scatter_gather_algorithms/scatter_gather.h
+++ scatter_gather_algorithms/scatter_gather.h
@@ -120,6 +120,86 @@ private:
     // writer to handle all the buckets created in phase 1 of sample sort
     OrderedFileWriter<T, SAMPLE_SORT_BUCKET_SIZE> intermediate_writer;
 
+    parlay::sequence<FileInfo>
+    ImprovedPhase2(const std::string &result_prefix, const ProcessorFunction &processor,
+                  const std::vector<FileInfo> &bucket_list) {
+        parlay::internal::timer timer("phase 2 internal");
+        const size_t num_files = bucket_list.size();
+        parlay::sequence<FileInfo> results(num_files, FileInfo("", 0, 0, 0));
+        std::atomic<size_t> current_file = 0, files_read = 0;
+        SimpleQueue<std::pair<size_t, T *>> read_queue, write_queue;
+        std::vector<std::thread> read_workers, write_workers;
+        read_queue.SetSizeLimit(256);
+        write_queue.SetSizeLimit(256);
+        for (size_t worker_thread = 0; worker_thread < 256; worker_thread++) {
+            read_workers.emplace_back([&]() {
+                while (true) {
+                    size_t index = current_file++;
+                    if (index >= num_files) {
+                        return;
+                    }
+                    const FileInfo& file = bucket_list[index];
+                    auto pointer = (T *) ReadEntireFile(file.file_name, file.file_size);
+                    read_queue.Push({index, pointer});
+                    if ((++files_read) == num_files) {
+                        read_queue.Close();
+                    }
+                }
+            });
+            write_workers.emplace_back([&]() {
+                while (true) {
+                    auto [res, code] = write_queue.Poll({0, nullptr});
+                    if (code == QueueCode::FINISH) {
+                        return;
+                    }
+                    auto [index, pointer] = res;
+                    FileInfo file = bucket_list[index];
+                    auto file_name = GetFileName(result_prefix, index);
+                    int fd = open(file_name.c_str(), O_WRONLY | O_DIRECT | O_CREAT, 0744);
+                    SYSCALL(fd);
+                    SYSCALL(write(fd, pointer, file.file_size));
+                    close(fd);
+                    free(pointer);
+                    results[index] = {file_name, bucket_list[index]};
+                }
+            });
+        }
+        timer.next("Worker threads created");
+        parlay::parallel_for(0, parlay::num_workers(), [&](size_t _) {
+            while (true) {
+                auto [res, code] = read_queue.Poll({0, nullptr});
+                if (code == QueueCode::FINISH) {
+                    return;
+                }
+                auto [index, pointer] = res;
+                size_t n = bucket_list[index].true_size / sizeof(T);
+                processor(&pointer, n);
+                write_queue.Push(std::pair(index, pointer));
+            }
+        }, 1);
+        write_queue.Close();
+        timer.next("All processor functions completed");
+        for (auto &worker: read_workers) {
+            worker.join();
+        }
+        for (auto &worker: write_workers) {
+            worker.join();
+        }
+        timer.next("Workers joined");
+        timer.stop();
+        return results;
+    }
+
+    parlay::sequence<FileInfo>
+    SimplePhase2(const std::string &result_prefix, const ProcessorFunction &processor,
+                 const std::vector<FileInfo> &bucket_list) {
+        return parlay::map(parlay::iota(bucket_list.size()), [&](size_t i) {
+            const auto& file_info = bucket_list[i];
+            auto result_name = GetFileName(result_prefix, i);
+            ProcessBucket(file_info, result_name, processor);
+            return FileInfo(result_name, file_info);
+        });
+    }
 public:
 
     std::vector<FileInfo> Run(std::vector<FileInfo> &input_files,
@@ -135,6 +215,7 @@ public:
         intermediate_writer.Initialize("spfx_", num_buckets, 1 << 20);
         timer.next("After sampling and before assign to bucket");
         std::vector<FileInfo> bucket_list;
+        CHECK(intermedia_io_threads < parlay::num_workers());
         parlay::par_do([&]() {
             parlay::parallel_for(0, intermedia_io_threads, [&](size_t i){
                 intermediate_writer.RunIOThread(&intermediate_writer);
@@ -148,16 +229,8 @@ public:
         });
         bucket_allocator::finish();
         timer.next("After assign to bucket and before phase 2");
-        // FIXME: should allow as much parallelism as internal memory allows; need to calculate memory budget
-        //   also, this can get tricky since using a conditional variable to guard against memory overflow
-        //   may result in suboptimal performance
-        parlay::sequence<FileInfo> results = parlay::map(parlay::iota(bucket_list.size()), [&](size_t i) {
-            auto file_info = bucket_list[i];
-            auto result_name = GetFileName(result_prefix, i);
-            ProcessBucket(file_info, result_name, processor);
-            return FileInfo(result_name, file_info);
-        });
-        timer.next("Sorting complete");
+        parlay::sequence<FileInfo> results = SimplePhase2(result_prefix, processor, bucket_list);
+        timer.next("Scatter gather complete");
         timer.stop();
         return {results.begin(), results.end()};
     }
