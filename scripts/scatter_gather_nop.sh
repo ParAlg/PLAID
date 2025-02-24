5ce6c286d70b3d9c02fb269c13945c4d038b706f
Peter Li <lihaohong6@gmail.com>
2025-02-23 21:46:29 -0500

    Unfinished script for scatter gather nop

---
+++ scripts/scatter_gather_nop.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/bash
+
+bazel build -c opt "//:speed_test"
+
+executable="./bazel-bin/speed_test"
+workload="scatter_gather_nop"
+
+# FIXME: unfinished script
+for i in {30..40}; do
+  "${executable}" "$workload" 37 "$num_buckets" > "temp.txt" 2>&1
+  printf "%s," "$num_buckets" >> "result.txt"
+  grep "Throughput" "temp.txt" | sed -e "s/^.*Throughput: \([0-9.]\+\)$/\1,/" >> "result.txt"
+done
+rm "temp.txt"
+cat "result.txt"
