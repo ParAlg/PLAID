85d511b998e9785816c8a06bffac4edb554040bb
Peter Li <lihaohong6@gmail.com>
2025-02-27 00:07:48 -0500

    In-memory sorting benchmarks (vary size)

--- scripts/bench_sort.sh
+++ scripts/bench_sort.sh
@@ -1,20 +1,48 @@
 #!/bin/bash
 
-bazel build -c opt "//:sample_sort"
+bazel build -c opt "//:all"
 
-executable="./bazel-bin/sample_sort"
-input_prefix="numbers"
-output_prefix="result"
-for i in {30..40}; do
-  bash scripts/clear.sh
-  "${executable}" gen "${i}" "${input_prefix}" 1
-  printf "%s," "${i}" >> "result.txt"
-  for _ in {1..5}; do
-    bash scripts/reset.sh
-    "${executable}" "run" "${input_prefix}" "${output_prefix}" > "temp.txt"
-    grep "DONE" "temp.txt" | sed -e "s/^.*DONE: \([0-9.]\+\)$/\1,/" | tr -d '\n' >> "result.txt"
-  done
-  echo "" >> "result.txt"
+for type in 0 2 3; do
+    echo "RNG type: $type" >> "result.txt"
+
+    if [ "$type" == 0 ]; then
+        param=0 # unbounded RNG
+    elif [ "$type" == 2 ]; then
+        param=1
+    elif [ "$type" == 3 ]; then
+        param=5
+    fi
+    if [ "$1" == "i" ]; then
+        executable="./bazel-bin/speed_test"
+        for data_size in $(seq 30 34); do
+            printf "%s," "$data_size" >> "result.txt"
+            "${executable}" "sorting_in_memory" "$data_size" "$type" "$param" > "temp.txt"
+            grep "DONE" "temp.txt" | sed -e "s/^.*DONE: \([0-9.]\+\)$/\1,/" | tr -d '\n' >> "result.txt"
+            echo "" >> "result.txt"
+        done
+    elif [ "$1" == "e" ]; then
+        executable="./bazel-bin/sample_sort"
+        input_prefix="numbers"
+        output_prefix="result"
+        for i in {30..40}; do
+            bash scripts/clear.sh
+            "${executable}" gen "${i}" "${input_prefix}" "$type" "$param"
+            printf "%s," "${i}" >> "result.txt"
+            loop_end=5
+            if [ "$data_size" -ge 34 ]; then
+                loop_end=1
+            fi
+            for _ in $(seq 1 "$loop_end"); do
+                bash scripts/reset.sh
+                "${executable}" "run" "${input_prefix}" "${output_prefix}" > "temp.txt"
+                grep "DONE" "temp.txt" | sed -e "s/^.*DONE: \([0-9.]\+\)$/\1,/" | tr -d '\n' >> "result.txt"
+            done
+            echo "" >> "result.txt"
+        done
+    else
+        echo "Usage: $0 [i|e]"
+        exit 1
+    fi
 done
 rm "temp.txt"
 cat "result.txt"
