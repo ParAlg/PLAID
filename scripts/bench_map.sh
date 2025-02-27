2be5c216bdf7fac1b3495e2d860f2140c8cd47c8
Peter Li <lihaohong6@gmail.com>
2025-02-27 17:50:59 -0500

    Incomplete map benchmark

---
+++ scripts/bench_map.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+bazel build -c opt "//:all"
+bin="./bazel-bin"
+
+if [ "$1" == "i" ]; then
+    echo "In-memory test"
+    for data_size in $(seq 30 34); do
+        printf "%s," "$data_size" >> "result.txt"
+        "$bin/speed_test" "map_in_memory" "$data_size" > "temp.txt" 2>&1
+        grep "DONE" "temp.txt" | sed -e "s/^.*DONE: \([0-9.]\+\)$/\1,/" | tr -d '\n' >> "result.txt"
+        grep "Throughput" "temp.txt" | sed -e "s/^.*Throughput: \([0-9.GB]\+\)$/\1,/" | tr -d '\n' >> "result.txt"
+        echo "" >> "result.txt"
+    done
+elif [ "$1" == "e" ]; then
+    echo "External memory test"
+    for data_size in $(seq 39 40); do
+        printf "%s," "$data_size" >> "result.txt"
+        ./scripts/clear.sh
+        "$bin/sample_sort" "gen" "$data_size" "numbers" 0 0
+        sudo ./scripts/fstrim.sh
+        sleep 5
+        "$bin/sequence" "map" "numbers" "result" > "temp.txt" 2>&1
+        grep "Time" "temp.txt" | sed -e "s/^.*Time: \([0-9.GB]\+\)$/\1,/" | tr -d '\n' >> "result.txt"
+        grep "Throughput" "temp.txt" | sed -e "s/^.*Throughput: \([0-9.GB]\+\)$/\1,/" | tr -d '\n' >> "result.txt"
+        echo "" >> "result.txt"
+    done
+else
+    echo "Usage: $0 [i|e]"
+    exit 1
+fi
+rm "temp.txt"
+cat "result.txt"
